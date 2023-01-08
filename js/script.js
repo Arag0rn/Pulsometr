1b95c7a82eed1d16a3ea0c64fba2af1b52566cda
Khabuzov Serhii <habuzovs@gmail.com>
2023-01-08 11:39:07 +0100

    added tabs and script

--- js/script.js
+++ js/script.js
@@ -14,4 +14,16 @@ $(document).ready(function(){
             },
         ]
       });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_activ)', function() {
+        $(this)
+          .addClass('catalog__tab_activ').siblings().removeClass('catalog__tab_activ')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_activ').eq($(this).index()).addClass('catalog__content_activ');
+      });
+      $('.catalog-item__link').each(function(i){
+        $(this).on('click', function(e){
+            e.preventDefault();
+            $('.catalog-item__content').toggleClass('catalog-item__content_activ');
+            $('.catalog-item__list').toggleClass('catalog-item__list_activ');
+        })
+      });
   });
